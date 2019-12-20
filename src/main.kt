5499b2b1f853af0ffa282ecbb58d250b79996a0e
Jordan Tranchina <Jordan12522@gmail.com>
2019-12-20 15:24:30 -0600

    Adding a slightly more complex string

--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,5 @@
 fun main(args: Array<String>) {
     println("Welcome to Kotlin Essential Training, ${args[0]}!")
     println("The first argument is " + args[0])
+    println("Hello " + args[0] + ", This is your computer speaking!")
 }
